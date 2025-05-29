05724e4134c0a1be1fa96914b725765555daf6b9
pdrzewski <pedro.martins@sptech.school>
2025-05-29 09:46:28 -0300

    mudanças session storage

--- src/models/graficoModel.js
+++ src/models/graficoModel.js
@@ -2,7 +2,7 @@ const database = require('../database/config');
 
 function buscarDados() {
     const instrucao = `
-        SELECT qtd_acertos FROM votos; 
+        SELECT v.id_usuario, v.qtd_acertos FROM votos v inner join usuario u on v.id_usuario = u.id; 
     `;
     return database.executar(instrucao);
 }
@@ -11,7 +11,7 @@ function inserirDados(acertos) {
     const instrucao = `
         INSERT INTO votos (qtd_acertos) VALUES (${acertos});
     `;
-    return database.executar(instrucao); // Adicionado return
+    return database.executar(instrucao);
 }
 
 module.exports = { buscarDados, inserirDados };
